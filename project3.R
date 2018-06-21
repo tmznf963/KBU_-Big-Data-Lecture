0f4e13827331889856bbcc3333e54e9536e1d1b8
tmznf963 <qhfhd963@naver.com>
2018-06-21 11:56:49 +0900

    update

--- project3.R
+++ project3.R
@@ -12,56 +12,44 @@ eq <- eq[, !names(eq) %in%
 
 str(eq)
 head(eq)
-#결측치 제거
-install.packages("dplyr")
-library(dplyr)
-eq<- na.omit(eq)
-eq
-
-
-eq
-str(eq)
-head(eq)
 View(eq)
 
-#level의 수가 너무 많은 것은 단순 문자열로 취급
-# eq$Date <- as.character(eq$Date)#Factor
-# eq$Time <- as.character(eq$Time)#Factor
-# eq$Latitude <- as.character(eq$Latitude)#num
-# eq$Longitude <- as.character(eq$Longitude)#num
-# eq$Depth <- as.character(eq$Depth)#num
-# eq$Magnitude <- as.character(eq$Magnitude)#Factor
+install.packages("bindrcpp")
+library(bindrcpp)
+# [ctrl + shit + M]으로 %>% 기호 입력
+# NE == 핵폭발 135행 데이터
+# eq2 == 지진 135행 데이터
+# earthquake 135행까지 뽑아내기
 
-str(eq)
+NE <- eq %>% filter(Type == "Nuclear Explosion") #타입이 핵폭발인 것만 추출
+eq <- eq %>% filter(Type == "Earthquake ") # 타입이 지진인 것만 추출
+eq2 <- eq[c(1:135),] # 핵폭발의 데이터와 갯수를 맞추기위해 135개의 행 넣기
+
+#데이터 확인
+head(NE)
+head(eq2)
+
+# 핵폭발 + 지진 데이터 합치기
+new_eq <- rbind(eq2,NE)
+View(new_eq)
+
+
+str(new_eq)
+
+#Factor levels가 많은 Date, Time의 변환
+new_eq$Date <- as.character(new_eq$Date)
+new_eq$Time <- as.character(new_eq$Time)
+
+str(new_eq)
 
 #Factor 수준(level)에 NA값 확인
-levels(eq$Type)
-table(eq$Type)
+levels(new_eq$Type)
+table(new_eq$Type)
 
-levels(eq$Magnitude.Type)
-table(eq$Magnitude.Type)
-
-table(eq$Date)
-table(eq$Time)
-table(eq$Latitude)
-table(eq$Longitude)
-table(eq$Depth)
+levels(new_eq$Magnitude.Type)
+table(new_eq$Magnitude.Type)
 
 
-# 빈 문자열 NA를 수정하기
-# table() 함수는 NA 값을 제외하고 값을 출력시키기에
-# useNA에 always를 지정해 NA에 대한 갯수도 출력하도록 하여 빈도를 확인
-# levels(eq$Magnitude.Type)[1] <- NA
-# table(eq$Magnitude.Type, useNA = 'always')
-
-
-# 마지막으로 str(), head(), View() 등의 명령어를 
-# 사용해 데이터가 잘 변경되었는지 확인
-levels(eq$Magnitude.Type)
-table(eq$Magnitude.Type)
-str(eq)
-head(eq)
-View(eq)
 
 #테스트 데이터 분리
 install.packages("caret")
@@ -70,17 +58,21 @@ library(caret)
 #난수 생성시 seed 지정, 교차 검증 createDataPartition() 사용
 #30%의 데이터는 test, 70%의 데이터는 train을 위해
 set.seed(137)
-test_idx <- createDataPartition(eq$Magnitude.Type, p=0.1)$Resample1
-eq.test <- eq[test_idx, ]
-eq.train <- eq[-test_idx, ]
+test_idx <- createDataPartition(new_eq$Type, p=0.1)$Resample1
+new_eq.test <- new_eq[test_idx, ]
+new_eq.train <- new_eq[-test_idx, ]
+NROW(new_eq.test)
+prop.table(table(new_eq.test$Type))
 
+NROW(new_eq.train) 
+prop.table(table(new_eq.train$Type))
 
 #의사결정나무의 형성
 install.packages("tree")
 library(tree)
-treemod <- tree(Magnitude.Type ~ Latitude + Longitude + Type + Depth + Magnitude , data=eq.train)
-treemod <- tree(Magnitude.Type ~ Type + Depth + Magnitude , data=eq.train)
-treemod <- tree(Magnitude.Type ~ Latitude + Longitude + Type  , data=eq.train)
+treemod <- tree(Type ~ Date + Time + Latitude + Longitude + Magnitude.Type + Depth + Magnitude , data=new_eq.train)
+treemod <- tree(Type ~ Magnitude.Type + Depth + Magnitude , data=new_eq.train)
+treemod <- tree(Type ~ Latitude + Longitude + Magnitude.Type  , data=new_eq.train)
 plot(treemod)
 text(treemod)
 
@@ -94,32 +86,27 @@ text(prune.trees, pretty = 0)
 #예측하기 & 모델 평가
 install.packages("e1071")
 library(e1071)
-treepred <- predict(prune.trees, eq.test, type='class')
-confusionMatrix(treepred, eq.test$Magnitude.Type)
+treepred <- predict(prune.trees, new_eq.test, type='class')
+confusionMatrix(treepred, new_eq.test$Type)
 
 #데이터 확인
-eq.test
-head(eq.test)
-str(eq.test)
-View(eq.test)
+new_eq.test
+head(new_eq.test)
+str(new_eq.test)
+View(new_eq.test)
 
-eq.train
-head(eq.train)
-str(eq.train)
-View(eq.train)
-
-NROW(eq.test)
-prop.table(table(eq.test$Magnitude.Type))
-
-NROW(eq.train) 
-prop.table(table(eq.train$Magnitude.Type))
+new_eq.train
+head(new_eq.train)
+str(new_eq.train)
+View(new_eq.train)
 
 
-#eq, eq.test, eq.train 데이터를 eq.RData 라는 파일에 저장
-save(eq, eq.test, eq.train, file="eq.RData")
+
+#new_eq, new_eq.test, new_eq.train 데이터를 new_eq.RData 라는 파일에 저장
+save(new_eq, new_eq.test, new_eq.train, file="new_eq.RData")
 
 #교차 검증 준비
-createFolds(eq.train$Magnitude.Type, k=10)
+createFolds(new_eq.train$Type, k=10)
 
 #Fold에는 검증 데이터로 사용할 데이터 번호가 저장
 
@@ -128,10 +115,10 @@ createFolds(eq.train$Magnitude.Type, k=10)
 #매번 같은 결과가 나오게 하기 위해 사용
 
 create_ten_fold_cv <- function(){
-  set.seed(1000)
-  lapply(createFolds(eq.train$Magnitude.Type, k=10),function(idx){
-    return(list(train=eq.train[-idx, ],
-                validation = eq.train[idx, ]))
+  set.seed(137)
+  lapply(createFolds(new_eq.train$Type, k=10),function(idx){
+    return(list(train=new_eq.train[-idx, ],
+                validation = new_eq.train[idx, ]))
   })
 }
 #함수는 Fold01, Fold02, ..., Fold10을 가진 리스트를 반환하며 
@@ -153,24 +140,24 @@ x[[1]]$train
 #데이터의 요약정보를 얻을 수 있는 기능
 install.packages("Hmisc")
 library(Hmisc)
-str(eq)
+str(new_eq)
 
-data = eq.train
+data = new_eq.train
+str(data)
+# Date + Time + Latitude + Longitude + Magnitude.Type + Depth + Magnitude
+# Latitude + Longitude + Magnitude.Type + Depth + Magnitude
 
-# Date + Time + Latitude + Longitude + Type + Depth + Magnitude
-# Latitude + Longitude + Type + Depth + Magnitude
-
-summary(Magnitude.Type ~ Latitude + Longitude + Type + Depth + Magnitude , data = data, method = "reverse")
+summary(Type ~ Latitude + Longitude + Magnitude.Type + Depth + Magnitude , data = data, method = "reverse")
 
 str(data)
-data = eq.train
+data = new_eq.train
 data
 
 #caret::featurePlot()을 사용해 데이터를 시각화
 #num 컬럼만 선택하여 X에 지정
 install.packages("ellipse")
 library(ellipse)
-str(eq)
+
 data.complete <- data[complete.cases(data),]
 featurePlot(
   data.complete[,
@@ -178,47 +165,48 @@ featurePlot(
                        function(n){
                          is.numeric(data.complete[, n])
                        })],
-  data.complete[, c("Magnitude.Type")],
+  data.complete[, c("Type")],
   "ellipse"
 )
 
-#모자이크 플롯 크기, 타입 별  크기타입
-mosaicplot(Magnitude.Type ~ Magnitude , data = data , color = TRUE,
-           main=" Magnitude")
+#모자이크 플롯 규모, 진폭유형 별 타입
+mosaicplot(Type ~ Magnitude + Magnitude.Type , data = data , color = TRUE,
+           main=" Magnitude and Magnitude.Type")
 
-mosaicplot(Magnitude.Type ~ Type , data = data , color = TRUE,
+mosaicplot(Type ~ Magnitude , data = data , color = TRUE,
            main=" Type")
+
+mosaicplot(Type ~ Magnitude.Type , data = data , color = TRUE,
+           main="Magnitude.Type")
 
 str(data)
 
-levels(eq$Magnitude.Type)
+levels(new_eq$Type)
 
 # Magnitude, Type 별 Magnitude.Type을 보고 싶다면
-xtabs( ~ Type + Magnitude, data=data)
+xtabs( ~ Magnitude.Type + Magnitude, data=data)
+xtabs( ~ Magnitude.Type, data=data)
 
-#[1] "MB"  "ML"  "MS"  "MW"  "MWB" "MWC" "MWR" "MWW"
-xtabs(Magnitude.Type == "MB" ~ Type + Magnitude,data=data)
-xtabs(Magnitude.Type == "ML" ~ Type + Magnitude,data=data)
-xtabs(Magnitude.Type == "MS" ~ Type + Magnitude,data=data)
-xtabs(Magnitude.Type == "MW" ~ Type + Magnitude,data=data)
-xtabs(Magnitude.Type == "MWB" ~ Type + Magnitude,data=data)
-xtabs(Magnitude.Type == "MWC" ~ Type + Magnitude,data=data)
-xtabs(Magnitude.Type == "MWR" ~ Type + Magnitude,data=data)
-xtabs(Magnitude.Type == "MWW" ~ Type + Magnitude,data=data)
+#[1] "Earthquake"        "Explosion"         "Nuclear Explosion"
+# Explosion == 갯수가 0개 skip
+xtabs(Type == "Earthquake" ~ Magnitude.Type + Magnitude,data=data)
+xtabs(Type == "Nuclear Explosion" ~ Type + Magnitude,data=data)
 
-# 타입,크기별 크기타입("MB")일 확률
-xtabs(Magnitude.Type == "MB" ~ Type + Magnitude,data=data) /
-  + xtabs( ~ Type + Magnitude,data=data)
+str(data)
 
+# 규모,진폭유형 별 타입이 ("Earthquake")일 확률
+# 규모,진폭유형 별 타입이 ("Nuclear Explosion")일 확률
+xtabs(Type == "Earthquake" ~ Magnitude.Type + Magnitude,data=data) /
+  + xtabs( ~ Magnitude.Type + Magnitude,data=data)
 
-str(eq)
+str(new_eq)
 #나무 모형 중 하나로 다양한 변수의 상호작용을 잘 표현
 #rpart는 이를 대리 변수(surrogate variable)로 처리
 library(rpart)
 m <- rpart(
-  Magnitude.Type ~ Latitude + Longitude + Type + Depth + Magnitude,
-  data=eq.train)
-p <- predict(m, newdata = eq.train, type="class")
+  Type ~ Latitude + Longitude + Magnitude.Type + Depth + Magnitude,
+  data=new_eq.train)
+p <- predict(m, newdata = new_eq.train, type="class")
 
 head(p)
 
@@ -232,11 +220,11 @@ library(foreach)
 folds <- create_ten_fold_cv()
 rpart_result <- foreach(f=folds) %do%{
   model_rpart <- rpart(#
-    Magnitude.Type ~ Latitude + Longitude + Type + Depth + Magnitude ,
+    Type ~ Latitude + Longitude + Magnitude.Type + Depth + Magnitude ,
     data=f$train)
   predicted <- predict(model_rpart, newdata = f$validation,#
                        type="class")
-  return(list(actual=f$validation$Magnitude.Type, predicted=predicted))
+  return(list(actual=f$validation$Type, predicted=predicted))
 }
 
 head(rpart_result)
@@ -253,23 +241,23 @@ evaluation <- function(lst){
     return(sum(one_result$predicted == one_result$actual)
            / NROW(one_result$actual))
   })
-  print(sprintf("MEAN +/- SD : %.3f +/- %.3f",
+  print(sprintf("정확도 +/- SD : %.3f 오차범위 +/- : %.3f",
                 mean(accuracy),sd(accuracy)))
   return(accuracy)
 }
 (rpart_accuracy <-evaluation(rpart_result))
 
-
+str(new_eq)
 #다른 모델링 기법을 적용
 install.packages("party")
 library(party)
 ctree_result <- foreach(f=folds) %do%{
   model_ctree <- ctree(
-    Magnitude.Type ~ Latitude + Longitude + Type + Depth + Magnitude,
+    Type ~ Latitude + Longitude + Magnitude.Type + Depth + Magnitude,
     data=f$train)
   predicted <- predict(model_ctree, newdata=f$validation,
                        type="response")
-  return(list(actual=f$validation$Magnitude.Type, predicted=predicted))
+  return(list(actual=f$validation$Type, predicted=predicted))
 }
 (ctree_accuracy <- evaluation(ctree_result))
 
@@ -279,52 +267,28 @@ plot(density(rpart_accuracy), main = "rpart")
 plot(density(ctree_accuracy), main = "ctree")
 
 plot(density(rpart_accuracy), main = "rpart VS ctree")
-lines(density(ctree_accuracy),col="red", lty ="dashed")
-lines(density(rpart_accuracy),col="red", lty ="dashed")
+lines(density(ctree_accuracy),col="black", lty ="solid")
+lines(density(ctree_accuracy),col="blue", lty ="dotted")
+lines(density(rpart_accuracy),col="green", lty ="dotted")
 
-str(eq)
+str(new_eq)
 
 
-# 또 다른 특징(Feature)의 발견
-# 검증 데이터를 분리하지 않은
-# eq.train 을 Date 에 따라 정렬해 표시
-View(eq.train[order(eq.train$Date),
-              c("Depth","Type","Magnitude")])
 
-#NA인 행의 수를 확인하는 코드
-sum(is.na(eq.train$Date))
-
-#Latitude + Longitude + Type + Depth + Magnitude
-Depth_result <- foreach(f=folds) %do%{
-  Depth_model_ctree <- ctree(
-    Magnitude.Type ~  Depth + Magnitude + Type,
-    data=f$train)
-  predicted <- predict(Depth_model_ctree, newdata=f$validation,
-                       type="response")
-  return(list(actual=f$validation$Magnitude.Type, predicted=predicted))
-}
-(Depth_accuracy <-evaluation(Depth_result))
-
-plot(density(rpart_accuracy), main = "rpart VS ctree")
-lines(density(ctree_accuracy),col="red", lty ="dashed")
-lines(density(Depth_accuracy),col="green", lty ="dashed")
-
-table(eq$Type)
-
-#eq$Latitude,eq$Longitude,eq$Depth,eq$Magnitude
-is.na(eq$Latitude)
-cor(eq$Latitude,eq$Longitude)
-cor(eq$Latitude,eq$Depth)
-cor(eq$Latitude,eq$Magnitude)
-cor(eq$Depth,eq$Magnitude)
-cor(eq$Longitude,eq$Depth)
+#new_eq$Latitude,new_eq$Longitude,new_eq$Depth,new_eq$Magnitude
+is.na(new_eq$Latitude)
+cor(new_eq$Latitude,new_eq$Longitude)
+cor(new_eq$Latitude,new_eq$Depth)
+cor(new_eq$Latitude,new_eq$Magnitude)
+cor(new_eq$Depth,new_eq$Magnitude)
+cor(new_eq$Longitude,new_eq$Depth)
 
 # 열의 피어슨상관계수 0.5 이상이면 관계가 높다.
-cor(eq.train[c("Latitude","Longitude","Depth","Magnitude")])
-symnum(cor(eq.train[c("Latitude","Longitude","Depth","Magnitude")]))
+cor(new_eq.train[c("Latitude","Longitude","Depth","Magnitude")])
+symnum(cor(new_eq.train[c("Latitude","Longitude","Depth","Magnitude")]))
 
 install.packages("corraram")
 library(corrgram)
-corrgram(cor(eq.train[c("Latitude","Longitude","Depth","Magnitude")]),type="corr",upper.panel = panel.conf)
+corrgram(cor(new_eq.train[c("Latitude","Longitude","Depth","Magnitude")]),type="corr",upper.panel = panel.conf)
 
 
